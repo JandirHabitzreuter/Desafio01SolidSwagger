b6581e4a6c2c39cfe4f7d3fa3f2c627132d0cca6
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2022-01-18 10:03:11 -0300

    documenting with swagger

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("User not a admin!");
+    }
+
+    const allUsers = this.usersRepository.list();
+    return allUsers;
+  }
+}
+
+export { ListAllUsersUseCase };
